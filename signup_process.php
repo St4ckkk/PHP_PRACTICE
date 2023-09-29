a184a3eb7acb2b53c2e5f9491765dc76fc21a312
Keyan Andy Delgado <Delgado_KA@TREKLAB.COM>
2023-09-30 07:38:28 +0800

    PHP ACTIVITY 1

---
+++ signup_process.php
@@ -0,0 +1,12 @@
+<?php
+    $uname = $_POST['uname'];
+    $pass = $_POST['pass'];
+    $email = $_POST['email'];
+    $usertype = $_POST['usertype'];
+
+
+    echo $uname . "<br>";
+    echo $pass . "<br>";
+    
+    
+?>
