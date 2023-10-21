93d56bee22d1227dfb3539050b60777070005ff5
Keyan Andy Delgado <Delgado_KA@TREKLAB.COM>
2023-10-21 08:57:14 +0800

    UPDATED

--- _handleLogin.php
+++ _handleLogin.php
@@ -9,11 +9,11 @@ $pass = md5($_POST['pass']);
 $sql = "SELECT * FROM users WHERE user_email = '$email' AND user_pass = '$pass' ";
 $result = $conn->query($sql);
 $count = mysqli_num_rows($result);
-if ($count == 1) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email']) && isset($_POST['pass'])) {
     $_SESSION['email'] = $email;
-    header("location: /LOGIN_REGISTRATION/dashboard.php");
+    header("location: /LOGIN_REGISTRATION/dashboard.php?loginsuccess=true");
 } else {
-    header("location: /LOGIN_REGISTRATION/login.php");
+    header("location: /LOGIN_REGISTRATION/login.php?loginsuccess=false");
 }
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 ?>
--- dashboard.php
+++ dashboard.php
@@ -1,52 +1,46 @@
+<?php
+include('C:/xampp/htdocs/LOGIN_REGISTRATION/databases/dbcon.php');
+include('session.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome <?php echo $row_session['username']?></title>
+  <title>Welcome <?php echo $row_session['username']; ?></title>
 
-  <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/@docsearch/css@3"
-    />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3" />
 
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-    />
-    <?php 
-      include ('session.php');
-    ?>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
 </head>
-  <style>
-    h1 {
-      text-align: center;
-      font-size: 2rem;
-    }
-  </style>
+<style>
+  h1 {
+    text-align: center;
+    font-size: 2rem;
+  }
+</style>
+
 <body>
 
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-6">
-         <label class="form-control">
+  <div class="container-fluid d-flex justify-content-center align-items-center">
+    <div class="row">
+      <div class="col-lg-12 mt-5">
+        <label class="form-control">
           Hello : <span class="text-primary">
-            <?php 
-              $row_session['username']
-            
+            <?php
+            echo $row_session['username'];
             ?>
           </span>
-         </label>
-         
-        </div>
-        <div class="col-6">
-
-        </div>
+        </label>
+      </div>
+      <div class="col-6">
+        <!-- Your content here -->
       </div>
     </div>
-  
+  </div>
 
-</body>
+</body
 
 </html>
